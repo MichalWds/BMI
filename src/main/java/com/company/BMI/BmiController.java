3c2f4e9090e1948a457223db42e64b9b1a90d412
MichalWds <michalwadas@onet.pl>
2019-04-02 23:24:40 +0200

    first

---
+++ src/main/java/com/company/BMI/BmiController.java
@@ -0,0 +1,39 @@
+package com.company.BMI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DecimalFormat;
+
+@Controller
+public class BmiController {
+
+
+    DecimalFormat df = new DecimalFormat("##");
+
+    @GetMapping("/bmi")
+    public String getParam(
+
+    ) {
+        return "bmi";
+    }
+
+    @GetMapping("/result")
+    public String getResult(@RequestParam Double height,
+                            @RequestParam Double weight,
+                            DecimalFormat df,
+                            ModelMap map) {
+
+        BmiCounter  bmiCounter = new BmiCounter(height, weight);
+        map.put("yourBMI", df.format(bmiCounter.getBMI()));
+        map.put("yourBM", bmiCounter.getMessage());
+
+        return "result";
+    }
+
+
+}
+
