d27968e7d6140dfaf2d02fd5ae3f5eb23a78cc10
MichalWds <michalwadas@onet.pl>
2019-04-15 11:34:41 +0200

    deleted empty lines

--- src/main/java/com/company/BMI/BmiCounter.java
+++ src/main/java/com/company/BMI/BmiCounter.java
@@ -5,12 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
-
 public class BmiCounter {
     private double height;
     private double weight;
-
 
     private String a = "Masz niedowagę, przytyj trochę!";
     private String b = "Bardzo dobrze! Masz odpowiednią wagę dla swojego wzrostu!";
@@ -21,40 +18,32 @@ public class BmiCounter {
         this.height = height;
         this.weight = weight;
     }
-
     public double getBMI() {
 
         return this.weight / (this.height * this.height / 10000);
-        
     }
 
     public String getMessage() {
-
         if (getBMI() < 18.5) {
-            return a ;
+            return a;
         } else if (getBMI() <= 24.9) {
-            return b ;
+            return b;
         } else if (getBMI() <= 29.9) {
-            return c ;
+            return c;
         } else {
             return d;
         }
-
-
     }
 
     private double getHeight() {
         return height;
     }
-
     private void setHeight(double height) {
         this.height = height;
     }
-
     private double getWeight() {
         return weight;
     }
-
     private void setWeight(double weight) {
         this.weight = weight;
     }
